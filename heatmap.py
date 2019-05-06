e1116d7241cf0df2e64d08665a4cff43a7c8b8db
stephenslater <stephen.slater95@gmail.com>
2019-05-06 05:38:40 -0400

    Final analytics version for tuning

--- heatmap.py
+++ heatmap.py
@@ -14,19 +14,27 @@ from pyspark.sql import SQLContext, SparkSession
 from pyspark.mllib.linalg import Vectors
 from pyspark.sql.functions import udf, array, avg, col, size, struct, lag, window, countDistinct, monotonically_increasing_id, collect_list
 import pyspark.sql.functions as F
-from pyspark.sql.types import DoubleType, IntegerType, StringType, ArrayType, LongType, FloatType
+from pyspark.sql.types import *
 from pyspark.sql.window import Window
 
 # Window size
-window_minutes = 20. 
+window_minutes = 10 
 fps = 2.
 
 spark = SparkSession.builder.getOrCreate()
 
 """# Load data"""
-directory = 'folder'
-df = spark.read.json(directory)
-df = df.orderBy('timestamp').show()
+directory = '20190506-083703-12'
+
+# Schema for JSON
+schema = StructType([StructField('bboxes', ArrayType(DoubleType()), True),
+                     StructField('scores', ArrayType(DoubleType()), True),
+                     StructField('timestamp', StringType(), True),
+                     StructField('pair_bboxes', ArrayType(DoubleType()), True)])
+
+df = spark.read.json(directory, schema=schema)
+df = df.orderBy('timestamp')
+df.show()
 
 """# Functions used for UDFs (including velocity and group size)"""
 def count(column):
@@ -36,14 +44,29 @@ def sum_vals(column):
     return float(sum(column))
   
 def avg_vals(columns):
-    return float(columns[0] / columns[1]) if columns[1] else 0.0
+    return float(columns[0]) / columns[1] if columns[1] else 0.0
   
-def get_center(values):
-    res = []
-    for i in range(int(len(values)/4)):
-        y1, x1, y2, x2 = values[4*i:4*i+4]
-        x_mean, y_mean = round(float(x1+x2)/2, 3), round(float(y1+y2)/2, 3)
-        res.extend([x_mean, y_mean])
+def get_centers(values):
+    # Input: [n, x_i, y_i, ..., x_j, y_j] with centers for both frames
+    n = int(values[0])
+    return values[1:2*n+1]
+
+def get_pair_centers(values):
+    # Input: [n, y1, x1, y2, x2, ...] for both frames
+    # n = number of people in first frame
+    # returns [n, x_i, y_i, ..., x_j, y_j]
+    n = int(values[0])
+    frame_A = values[1:n*4+1]
+    frame_B = values[n*4+1:]
+    res = [n]
+    # Alternatively, could process these all as one array, since we store n
+    for frame in [frame_A, frame_B]:
+        res_curr = []
+        for i in range(int(len(frame)/4)):
+            y1, x1, y2, x2 = frame[4*i:4*i+4]
+            x_mean, y_mean = round(float(x1+x2)/2, 3), round(float(y1+y2)/2, 3)
+            res_curr.extend([x_mean, y_mean])
+        res.extend(res_curr)
     return res
 
 def get_x(values):
@@ -56,9 +79,12 @@ def fudf(val):
     return reduce(lambda x, y:x+y, val)
 
 def compute_velocities(cols, fps=2.0, threshold=0.3, return_assignments=False):
-    # Assume frame_1 = [x_i, y_i, x_{i+1}, y_{i+1}, ...]
-    f_1 = iter(cols[0])
-    f_2 = iter(cols[1])
+    # Assume cols = [n, x_i, y_i, ..., x_j, y_j] for centers (x_i, y_i)
+    # n = number of people in first frame
+    n = int(cols[0])
+
+    f_1 = iter(cols[1:2*n+1])
+    f_2 = iter(cols[2*n+1:])
     frame_1 = list(zip(f_1, f_1))
     frame_2 = list(zip(f_2, f_2))
     
@@ -149,13 +175,13 @@ def compute_groups(positions, threshold=0.1):
 
 count_udf = udf(count, IntegerType())
 sum_udf = udf(sum_vals, DoubleType())
-avg_udf = udf(lambda arr: avg_vals(arr), DoubleType())
-center_udf = udf(get_center, ArrayType(FloatType()))
-velocity_udf = udf(lambda arr: compute_velocities(arr), ArrayType(DoubleType()))
+center_udf = udf(get_centers, ArrayType(FloatType()))
+pair_center_udf = udf(get_pair_centers, ArrayType(FloatType()))
+velocity_udf = udf(compute_velocities, ArrayType(FloatType()))
 group_udf = udf(compute_groups, ArrayType(IntegerType()))
 x_udf = udf(get_x, ArrayType(DoubleType()))
 y_udf = udf(get_y, ArrayType(DoubleType()))
-flattenUdf = udf(fudf, ArrayType(DoubleType()))
+flatten_udf = udf(fudf, ArrayType(DoubleType()))
 
 """# Window size for pairwise shifting"""
 
@@ -164,16 +190,16 @@ w_pair = Window().partitionBy().orderBy(col("timestamp"))
 """# Create and Modify Columns"""
 
 df = (df.withColumn('num_people', count_udf('scores'))
-        .withColumn('centers', center_udf('bboxes'))
+        .withColumn('pair_centers', pair_center_udf('pair_bboxes'))
+        .withColumn('centers', center_udf('pair_centers'))
         .withColumn('x_centers', x_udf('centers'))
         .withColumn('y_centers', y_udf('centers'))
         .withColumn('group_sizes', group_udf('centers'))
         .withColumn('num_groups', count_udf('group_sizes'))
-        .withColumn('next_frame_centers', lag("centers", -1).over(w_pair)).na.drop()
-        .withColumn('velocities', velocity_udf(struct('centers', 'next_frame_centers')))
+       # .withColumn('next_frame_centers', lag("centers", -1).over(w_pair)).na.drop()
+        .withColumn('velocities', velocity_udf('pair_centers'))
         .withColumn('num_velocities', count_udf('velocities'))
         .withColumn('sum_velocities', sum_udf('velocities')))
-        
 df.show()
 
 """# Aggregate each 5 minute window to compute:
@@ -192,17 +218,10 @@ agg_df = (df.groupBy(window('timestamp', windowDuration=window_str, slideDuratio
                 avg('num_people'),
                 collect_list('x_centers'),
                 collect_list('y_centers'))
-           .withColumn('avg_group_size', avg_udf(struct('sum(num_people)', 'sum(num_groups)')))
-           .withColumn('avg_velocity', avg_udf(struct('sum(sum_velocities)', 'sum(num_velocities)')))
-           .withColumnRenamed('avg(num_people)', 'avg_num_people')
-           .withColumn('x_centers', flattenUdf('collect_list(x_centers)'))
-           .withColumn('y_centers', flattenUdf('collect_list(y_centers)'))
+           .withColumn('x_centers', flatten_udf('collect_list(x_centers)'))
+           .withColumn('y_centers', flatten_udf('collect_list(y_centers)'))
            .drop('collect_list(x_centers)')
            .drop('collect_list(y_centers)')
-           .drop('sum(num_people)')
-           .drop('sum(num_groups)')
-           .drop('sum(sum_velocities)')
-           .drop('sum(num_velocities)')
            .orderBy('window'))
 
 agg_df.show()
