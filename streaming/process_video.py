7c76e3fa97067592c6bb80e5fb2301751a0aa69e
Raymond Lin <raylin1000@gmail.com>
2019-05-07 01:12:23 +0000

    [streaming] Add analytics computations to video processing

--- streaming/process_video.py
+++ streaming/process_video.py
@@ -12,17 +12,12 @@ import cv2
 from pyspark.sql.types import *
 from pyspark.sql import SparkSession
 
-os.environ['PYSPARK_PYTHON'] = '/usr/bin/python3'
-os.environ['PYSPARK_DRIVER_PYTHON'] = '/usr/bin/python3'
-
-
-spark = SparkSession.builder.getOrCreate()
 sess = tf.Session()
 graph = tf.get_default_graph()
 
 MODEL_PATH = os.path.join(os.environ['HOME'], "models")
 VIDEO_PATH = os.path.join(os.environ['HOME'], "streaming-videos")
-OUTPUT_PATH = os.path.join(os.environ['HOME'], "streaming-dataframes")
+OUTPUT_PATH = os.path.join(os.environ['HOME'], "streaming-output")
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-m', '--model', required=True, dest='model', type=str,
@@ -50,8 +45,95 @@ def pred_from_frame(frames):
     bboxes, scores, n, classes = output['bboxes'], output['scores'], output['n'], output['classes']
     return bboxes, scores, n, classes
 
+def compute_center(bbox):
+    print(bbox)
+    y1, x1, y2, x2 = bbox
+    return round(float(x1+x2)/2, 3), round(float(y1+y2)/2, 3)
 
-def process_video(video_path, batch_size=32, rate=6):
+def dfs_all(graph):
+    def dfs(node, graph):
+        stack = [node]
+        cc = [node]
+        while stack:
+            u = stack.pop()
+            for v in graph[u]:
+                if not visited[v]:
+                    visited[v] = True
+                    cc.append(v)
+                    stack.append(v)
+        return cc
+
+    ccs = []
+    visited = [False for _ in range(len(graph))]
+    for i in range(len(graph)):
+        if not visited[i]:
+            visited[i] = True
+            cc = dfs(i, graph)
+            ccs.append(cc)
+    return list(map(len, ccs))
+
+def compute_groups(positions, threshold=0.1):
+    # Compute pairwise distances
+    graph = {i: set() for i in range(len(positions))}
+    for i in range(len(positions)):
+        for j in range(i, len(positions)):
+            # Euclidean distance between two people
+            dist = np.sqrt((positions[i][0] - positions[j][0]) ** 2 + (positions[i][1] - positions[j][1]) ** 2)
+            # Add edge to graph 
+            if dist < threshold:
+                graph[i].add(j)
+                graph[j].add(i)
+    lengths = dfs_all(graph)           
+    return lengths
+
+def compute_velocities(paired_centers, fps=2.0, threshold=0.3, return_assignments=False):
+    frame_1, frame_2 = paired_centers
+    
+    val_1 = {k: v for k, v in enumerate(frame_1)}
+    val_2 = {k: v for k, v in enumerate(frame_2)}
+
+    # Compute pairwise distances
+    distances = {}
+    for i in range(len(frame_1)):
+        for j in range(len(frame_2)):
+            # Euclidean distance between two people
+            distances[i, j] = np.sqrt(
+                (val_1[i][0] - val_2[j][0]) ** 2 + (val_1[i][1] - val_2[j][1]) ** 2)
+
+    # Assigned ids from frame 1 (reference frame), {id_i: vel_i}
+    velocities = dict()
+
+    # Assigned ids from frame 2 (with values as match in frame 1)
+    targets = dict()
+    num_assigned = 0
+    num_ids = min(len(frame_1), len(frame_2))
+
+    # Sort distances by key: (id in frame 1, id in frame 2)
+    pairs = sorted(distances.items(), key=lambda v:v[1])
+    for p, dist in pairs:
+        # Stop assigning ids when the distance exceeds a user-defined threshold
+        # i.e. this covers the case when a person leaves one end of the image
+        # and another person enters at the opposite side. We should not match
+        # these ids to each other.
+        if dist > threshold:
+            break
+
+        # Found closest ids between frames
+        if p[0] not in velocities and p[1] not in targets and num_assigned < num_ids:
+            num_assigned += 1
+            # Velocity (distance units per second)
+            velocities[p[0]] = dist * fps
+            targets[p[1]] = p[0]
+
+    if return_assignments:
+        assignments = [[v, k] for k, v in targets.items()]
+        return [float(v) for v in velocities.values()], assignments
+    return [float(v) for v in velocities.values()]
+
+def compute_avg(vals):
+    return sum(vals) / len(vals)
+
+def process_video(video_path, batch_size=32, rate=1):
     split_name = os.path.splitext(os.path.basename(video_path))[0].split('-')
     timestamp = '-'.join(split_name[:-1])
     fps = int(split_name[-1])
@@ -102,38 +184,28 @@ def make_predictions(videoname):
     
     # some cleaning
     agg_bboxes = []
-    agg_scores = []
     num_frames = len(bboxes) 
     for ind in range(num_frames):
         image_n = int(n[ind])
         image_bboxes = bboxes[ind][:image_n]
-        image_scores = scores[ind][:image_n]
         image_classes = classes[ind][:image_n]
-        indices = image_classes == 1.
-        image_bboxes = image_bboxes[indices]
-        image_scores = image_scores[indices]
-        agg_bboxes.append(image_bboxes.flatten().tolist())
-        agg_scores.append(image_scores.tolist())
+        image_bboxes = image_bboxes[image_classes == 1.]
+        agg_bboxes.append(image_bboxes.tolist())
     
-    num_detections = [float(len(x)) for x in agg_scores]
-    pair_bboxes = [[i] + j + k for i, j, k in zip(*(num_detections[:-1], agg_bboxes[:-1], agg_bboxes[1:]))]
+    num_detections = [float(len(x)) for x in agg_bboxes][:-1]
     agg_bboxes = agg_bboxes[:-1]
-    agg_scores = agg_scores[:-1]    
+    centers = [list(map(compute_center, bboxes)) for bboxes in agg_bboxes] 
+    pair_centers = list(zip(*(centers[:-1], centers[1:])))
+    group_size = list(map(compute_groups, centers))
+    velocities = list(map(compute_velocities, pair_centers))      
+    avg_group_size = list(map(compute_avg, group_size))
+    avg_velocities = list(map(compute_avg, velocities))   
     
-    # Save to Spark dataframe
-    output_dir = os.path.join(OUTPUT_PATH, os.path.splitext(videoname)[0] + '-%d' % RATE)
-    schema = StructType([StructField('bboxes', ArrayType(DoubleType()), True),
-                         StructField('scores', ArrayType(DoubleType()), True),
-                         StructField('timestamp', StringType(), True),
-                         StructField('pair_bboxes', ArrayType(DoubleType()), True)])
-    df = spark.createDataFrame(list(zip(*(agg_bboxes, agg_scores, timestamps, pair_bboxes))), schema)
-    df.coalesce(1).write.mode('overwrite').json(output_dir)
-
+    # TODO: Add code to visualize/otherwise use above computed values here
+    
     end_time = time.time()
     print("Total time: {} seconds".format(time.time() - start_time))
     
-#    df1 = spark.read.json(output_dir)
-#    df1.show() 
             
 while True:
     videos = sorted(os.listdir(VIDEO_PATH))
@@ -142,4 +214,4 @@ while True:
     for filename in videos:
         print('Processing video %s' % filename)
         make_predictions(filename)
-        os.remove(os.path.join(VIDEO_PATH, filename))	
+        # os.remove(os.path.join(VIDEO_PATH, filename))	
